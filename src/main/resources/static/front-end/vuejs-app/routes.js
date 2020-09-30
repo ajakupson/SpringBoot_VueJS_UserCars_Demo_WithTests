5dfdd3fe5d182d25af0d48af6772d556a00c6467
ajakupson <ajakupson@users.noreply.github.com>
2020-09-30 17:20:02 +0300

    Update routes.js

--- src/main/resources/static/front-end/vuejs-app/routes.js
+++ src/main/resources/static/front-end/vuejs-app/routes.js
@@ -1,8 +1,8 @@
 export default [
 	{'regEx': /^\/$/, 'view': 'Home'},
 	{'regEx': /^\/users$/, 'view': 'Users'},
-	{'regEx': /^\/users\/[1-9]+$/, 'view': 'UserDetails'},
-	{'regEx': /^\/users\/[1-9]+\/cars$/, 'view': 'UserCars'},
+	{'regEx': /^\/users\/\d+$/, 'view': 'UserDetails'},
+	{'regEx': /^\/users\/\d+\/cars$/, 'view': 'UserCars'},
 	{'regEx': /^\/cars$/, 'view': 'Cars'},
-	{'regEx': /^\/cars\/[1-9]+$/, 'view': 'CarDetails'}
+	{'regEx': /^\/cars\/\d+$/, 'view': 'CarDetails'}
 ]
